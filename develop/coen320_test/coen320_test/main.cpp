ecc2c7b64d983c54a06c1aaa5269ab18496753b2
Azoies1 <thomasle98@outlook.com>
2020-11-07 15:33:21 -0500

    Added CSV file reader to the VS project

--- develop/coen320_test/coen320_test/main.cpp
+++ develop/coen320_test/coen320_test/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
+#include "csvReader.h"
 using namespace std;
 
 int main() {
 
 	cout << "hello world\n";
+	csvReader reader = csvReader();
+	vector<dataLineModel> model = reader.read_csv("./DrivingData.csv");
 
 	return 0;
 }
